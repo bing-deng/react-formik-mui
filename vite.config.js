11425e2c9df3b358395f0dc89be607880590ca6b
bing-deng <804433988@qq.com>
2025-02-14 14:33:01 +0900

    init

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// 检查是否是 GitHub Pages 环境
+const isGitHubPages = process.env.GITHUB_PAGES === 'true'
+
 export default defineConfig({
   plugins: [react()],
+  base: isGitHubPages ? '/react-formik-mui/' : '/', // 替换 'ealite-json-page' 为您的仓库名
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
 }) 
